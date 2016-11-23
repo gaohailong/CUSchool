47428de9dc8643b89c338050c0803a5ba4680a87
gaohailong <190859080@qq.com>
2016-11-23 09:42:16 +0800

    完成校长致辞

--- src/main/webapp/background/self/js/title.js
+++ src/main/webapp/background/self/js/title.js
@@ -236,6 +236,63 @@ function confirmDeleteGraduation(tid) {
     }
 }
 
+//添加校长致辞
+function addPrincipal() {
+    var content = getContentByUeditor();
+    var head = $("#t-notification").val();
+    if (content == null) {
+        alert("未输入任何内容！");
+    } else if (head == null) {
+        alert("请输入文章标题！");
+    } else {
+        var data = {"notificationHead": head, "notificationContent": content};
+        $.ajax({
+            type: "POST",
+            url: "addSchoolPrincipal.action",
+            data: data,
+            dataType: "text",
+            success: function (data) {
+                window.location.href = "/background/success.jsp";
+            },
+            error: function (error) {
+                window.location.href = "/background/fail.jsp";
+            }
+        });
+    }
+}
+
+//修改校长致辞
+function updatePrincipal(id) {
+    var content = getContentByUeditor();
+    var head = $("#t-notification").val();
+    if (content == null) {
+        alert("未输入任何内容！");
+    } else if (head == null) {
+        alert("请输入文章标题！");
+    } else {
+        var data = {"notificationHead": head, "notificationContent": content};
+        $.ajax({
+            type: "POST",
+            url: "updateSchoolPrincipal.action?tid="+id,
+            data: data,
+            dataType: "text",
+            success: function (data) {
+                window.location.href = "/background/success.jsp";
+            },
+            error: function (error) {
+                window.location.href = "/background/fail.jsp";
+            }
+        });
+    }
+}
+
+//删除校长致辞
+function confirmDeletePrincipal(tid) {
+    if (confirm("确定要删除这篇文章吗？")) {
+        location.href = 'deleteSchoolPrincipal.action?tid=' + tid;
+    }
+}
+
 //激活当前页
 function pageActive() {
 
